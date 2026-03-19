b4184056d2fe63da062528cf0de3d8f624784330
vinitgharad <vinitgharad@gmail.com>
2026-03-19 20:10:36 +0530

    Delete PPL_Assignment/Assignment-2 directory

--- PPL_Assignment/Assignment-2/add_two.py
+++
@@ -1,6 +0,0 @@
-# Program to add two numbers
-a = float(input("Enter first number: "))
-b = float(input("Enter second number: "))
-
-sum_result = a + b
-print(f"The sum of {a} and {b} is {sum_result}")
